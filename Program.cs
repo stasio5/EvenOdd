6671dd6ecd5a50813036c79e9b3f86e594b2bd6d
stasio <stasio5@poczta.onet.pl>
2023-04-20 13:43:39 +0200

    Przed usunięciem zakresu ograniczającego wprowadzaną liczbę (GetUserInput(-100,100);)

--- Program.cs
+++ Program.cs
@@ -13,13 +13,31 @@ namespace EvenOdd
             var userEntereredNumber = 0;
             try
             {
-
+                Console.Write($"Podaj liczbę: ");
+                userEntereredNumber = GetUserInput(-100,100);
+                if (userEntereredNumber % 2 == 0) Console.Write("Liczba parzysta");
+                else Console.Write("Liczba nieparzysta");
             }
             catch (Exception)
             {
 
                 throw;
             }
+            Console.ReadKey();
+        }//Main()
+
+        private static int GetUserInput(int minNumber, int maxNumber)
+        {
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                    Console.Write($"Wprowadzona wartość jest nieprawidłowa. \nPodaj proszę liczbę z zakresu {minNumber} do {maxNumber}: ");
+                else if (number < minNumber || number > maxNumber)
+                    Console.Write($"Wprowadzona liczba jest poza zakresem. \nPodaj proszę liczbę z zakresu {minNumber} do {maxNumber}: ");
+                else
+                    return number;
+            }
+            while (true);
         }
     }
 }
